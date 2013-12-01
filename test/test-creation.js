847e00bd59da13a94ca9cd8198cd9c1677698e84
Zeno Rocha <zeno.rocha@liferay.com>
2013-12-01 01:31:19 -0800

    Update files expected on tests

--- test/test-creation.js
+++ test/test-creation.js
@@ -20,18 +20,22 @@ describe('devtools-theme generator', function () {
 
   it('creates expected files', function (done) {
     var expected = [
-      // add files you expect to exist here.
-      '.jshintrc',
-      '.editorconfig'
+      // add files you expect to exist here.,
+      '.editorconfig',
+      'devtools.html',
+      'devtools.js',
+      'manifest.json',
+      'styles.css',
     ];
 
     helpers.mockPrompt(this.app, {
-      'someOption': true
+      'themeName': true
     });
-    this.app.options['skip-install'] = true;
+
     this.app.run({}, function () {
       helpers.assertFiles(expected);
       done();
     });
+
   });
 });
