5385f0570c1c411bf9f90e9011a06117c0e748b1
Zeno Rocha <zeno.rocha@liferay.com>
2013-12-01 01:01:38 -0800

    Add theme name as a prompt question

--- app/index.js
+++ app/index.js
@@ -1,15 +1,11 @@
 'use strict';
+
 var util = require('util');
 var path = require('path');
 var yeoman = require('yeoman-generator');
 
-
 var DevtoolsThemeGenerator = module.exports = function DevtoolsThemeGenerator(args, options, config) {
   yeoman.generators.Base.apply(this, arguments);
-
-  this.on('end', function () {
-    this.installDependencies({ skipInstall: options['skip-install'] });
-  });
 
   this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
 };
@@ -19,32 +15,26 @@ util.inherits(DevtoolsThemeGenerator, yeoman.generators.Base);
 DevtoolsThemeGenerator.prototype.askFor = function askFor() {
   var cb = this.async();
 
-  // have Yeoman greet the user.
   console.log(this.yeoman);
 
   var prompts = [{
-    type: 'confirm',
-    name: 'someOption',
-    message: 'Would you like to enable this option?',
-    default: true
+    name: 'themeName',
+    message: 'What\'s the theme name?'
   }];
 
   this.prompt(prompts, function (props) {
-    this.someOption = props.someOption;
+    this.themeName = props.themeName;
 
     cb();
   }.bind(this));
 };
 
 DevtoolsThemeGenerator.prototype.app = function app() {
-  this.mkdir('app');
-  this.mkdir('app/templates');
-
-  this.copy('_package.json', 'package.json');
-  this.copy('_bower.json', 'bower.json');
+  this.copy('_manifest.json', 'manifest.json');
 };
 
 DevtoolsThemeGenerator.prototype.projectfiles = function projectfiles() {
+  this.copy('devtools.html', 'devtools.html');
+  this.copy('devtools.js', 'devtools.js');
   this.copy('editorconfig', '.editorconfig');
-  this.copy('jshintrc', '.jshintrc');
 };
